51670baa112a85f79a6f9ec8dc62758b6c9819ef
John Deng <john.deng@qq.com>
2019-01-11 22:44:05 +0800

    added debug log

--- app.js
+++ app.js
@@ -33,8 +33,11 @@ var casClient = new ConnectCas({
     },
     redirect: function(req, res) {
         // 在redirect中， 根据是否有特殊cookie来决定是否跳走
+        console.log('======= req ========')
         console.log(req)
+        console.log('======= res ========')
         console.log(res)
+        console.log('======= end ========')
         if (req.cookies.logoutFrom) {
           // 返回您想要重定向的路径
           return url.parse(req.cookies.logoutFrom).pathname;
